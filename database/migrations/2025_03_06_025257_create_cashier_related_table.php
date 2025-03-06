28c20ce28da00f27f03b30187a840b4ad4a48eb6
nondotxyz <shippudennaruto668@gmail.com>
2025-03-06 10:09:25 +0700

    feat: migration infinite

---
+++ database/migrations/2025_03_06_025257_create_cashier_related_table.php
@@ -0,0 +1,56 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+
+
+        Schema::create('cashier', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('telepon');
+            $table->string('email');
+            $table->timestamps();
+        });
+
+        Schema::create('sales', function (Blueprint $table) {
+            $table->id();
+            $table->integer('total');
+            $table->foreignId('sales_by')->constrained('cashier');
+            $table->timestamps();
+        });
+
+        Schema::create('product', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->integer('price');
+            $table->integer('stock');
+            $table->timestamps();
+        });
+
+        Schema::create('sales_detail', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('product_id')->constrained('products')->onDelete('cascade');
+            $table->foreignId('sale_id')->constrained('sales')->onDelete('cascade');
+            $table->integer('quantity');
+            $table->integer('sub_total');
+            $table->timestamps();
+        });
+
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('cashier_related');
+    }
+};
